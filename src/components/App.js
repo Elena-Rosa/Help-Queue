96960bc31744b2df99935c5940aa5fe653277f26
Elena <elenarosamail@gmail.com>
2024-01-04 12:05:31 -0800

    basic build working

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Header from "./Header;";
+import Header from "./Header";
 import TicketList from "./TicketList";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
+
   return (
     <React.Fragment>
       <Header />
